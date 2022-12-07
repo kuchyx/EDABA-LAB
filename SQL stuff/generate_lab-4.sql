6e05b74cc1e932fa9f07e09867993d2a0f6be839
Maciekxdabu <40292375+Maciekxdabu@users.noreply.github.com>
2022-12-07 12:56:44 +0100

    even more data generation

--- SQL stuff/generate_lab-4.sql
+++ SQL stuff/generate_lab-4.sql
@@ -1,5 +1,7 @@
 truncate table stadium;--
 truncate table competition;--
+truncate table manager;--
+truncate table club;--
 
 --create table countryTable (Cname varchar(20))
 --
@@ -19,33 +21,51 @@ insert into competition
 --into competionTemp 
 from dual
 connect by level <=10
---generate 10 records
 ;
 
-inster into club
-    select 
-   level +(select nvl(max(id),0) from competition) as id,
-    DBMS_RANDOM.value(0,1000000) as prize,
-    dbms_random.string('A', 6) as country,
---    (select Cname from countryTable order by random() limit 1) as country,
-    DBMS_RANDOM.value(1,5) as reputation
---into competionTemp 
-from dual
-connect by level <=10
---generate 10 records
+
+
+insert all
+into manager
+(id, skill, reputation, age, wage, club_id)
+            values 
+           (1 +(select nvl(max(id),0) from manager),
+            DBMS_RANDOM.value(1,5),
+            DBMS_RANDOM.value(1,5),
+            DBMS_RANDOM.value(18,80),
+            DBMS_RANDOM.value(0,1000000),
+            s_club_id)
+into club
+(id, budget, training_ground_quality, reputation, country_of_origin, competition_id, manager_id)
+    values 
+   (1 +(select nvl(max(id),0) from club),
+    DBMS_RANDOM.value(0,1000000),
+    DBMS_RANDOM.value(1,5),
+    DBMS_RANDOM.value(1,5),
+    dbms_random.string('A', 6),
+    (SELECT id FROM competition SAMPLE(1) WHERE rownum = 1), --https://stackoverflow.com/questions/27879874/how-to-get-random-foreign-key-in-sql-developer
+    s_manager_id)
+select s_club_id, s_manager_id
+from
+(
+    select
+        (1 +(select nvl(max(id),0) from club)) s_club_id,
+        (1 +(select nvl(max(id),0) from manager)) s_manager_id
+    from dual
+)
+--connect by level <=10
 ;
 
-insert into stadium
-    select 
-   level +(select nvl(max(id),0) from stadium) as id,
-    DBMS_RANDOM.value(0,40000) as capacity,
-    DBMS_RANDOM.value(1,5) as reputation,
-    dbms_random.string('A', 6) as location,
-    DBMS_RANDOM.value(1900,2100) as build_year,
-    DBMS_RANDOM.value(0,1000000) as value,
-    DBMS_RANDOM.value(0,200) as ticket_price,
-    level +(select nvl(max(id),0) from stadium) as club_id
-from dual
-connect by level <=10
---generate 10 records
-;
+--insert into stadium
+--    select 
+--   level +(select nvl(max(id),0) from stadium) as id,
+--    DBMS_RANDOM.value(0,40000) as capacity,
+--    DBMS_RANDOM.value(1,5) as reputation,
+--    dbms_random.string('A', 6) as location,
+--    DBMS_RANDOM.value(1900,2100) as build_year,
+--    DBMS_RANDOM.value(0,1000000) as value,
+--    DBMS_RANDOM.value(0,200) as ticket_price,
+--    level +(select nvl(max(id),0) from stadium) as club_id
+--from dual
+--connect by level <=10
+--;
