22aeff1087eb3c71f8483971d50d327e25be247e
Maciekxdabu <40292375+Maciekxdabu@users.noreply.github.com>
2022-12-14 12:02:52 +0100

    working manager-club generation

--- SQL stuff/generate_lab-4.sql
+++ SQL stuff/generate_lab-4.sql
@@ -1,7 +1,13 @@
-truncate table stadium;--
-truncate table competition;--
-truncate table manager;--
-truncate table club;--
+alter table manager
+DISABLE constraint manager_club_fk;
+
+alter table club
+DISABLE constraint club_manager_fk;
+
+truncate table stadium cascade;--
+truncate table manager cascade;--
+truncate table club cascade;--
+truncate table competition cascade;--competition is last since it has no foreign keys (but club holds a foreign key to competition)
 
 --create table countryTable (Cname varchar(20))
 --
@@ -23,7 +29,31 @@ from dual
 connect by level <=10
 ;
 
+insert into manager
+    select
+    level +(select nvl(max(id),0) from manager) as id,
+        DBMS_RANDOM.value(1,5) as skill,
+        DBMS_RANDOM.value(1,5) as reputation,
+        DBMS_RANDOM.value(18,80) as age,
+        DBMS_RANDOM.value(0,1000000) as wage,
+        level as club_id
+from dual
+connect by level <=10
+;
 
+insert into club
+--(id, budget, training_ground_quality, reputation, country_of_origin, competition_id, manager_id)
+    select
+   level +(select nvl(max(id),0) from club) as id,
+    DBMS_RANDOM.value(0,1000000) as budget,
+    DBMS_RANDOM.value(1,5)as training_ground_quality,
+    DBMS_RANDOM.value(1,5) as reputation,
+    dbms_random.string('A', 6) as country_of_origin,
+    (SELECT id FROM competition SAMPLE(1) WHERE rownum = 1) as competition_id, --https://stackoverflow.com/questions/27879874/how-to-get-random-foreign-key-in-sql-developer
+    level as manager_id
+from dual
+connect by level <=10
+;
 
 --insert all
 --into manager
@@ -85,17 +115,23 @@ connect by level <=10
 --;
 
 
-insert into player 
-    select 
-        level +(select nvl(max(id),0) from player) as id,
-        DBMS_RANDOM.value(1, 5) as skill,
-        dbms_random.string('A', 6) as position,
-        DBMS_RANDOM.value(1, 5) as reputation,
-        dbms_random.string('A', 6) as contract_status,
-        DBMS_RANDOM.value(0, 700) as injuries,
-        DBMS_RANDOM.value(15, 50) as age,
-        DBMS_RANDOM.value(15, 50) as wages,
-        DBMS_RANDOM.value(0, 1000000000) as transfer_value,
-        (SELECT id FROM CLUB SAMPLE(1) WHERE rownum = 1) as club_id
-    from dual 
-connect by level <=10
+--insert into player 
+--    select 
+--        level +(select nvl(max(id),0) from player) as id,
+--        DBMS_RANDOM.value(1, 5) as skill,
+--        dbms_random.string('A', 6) as position,
+--        DBMS_RANDOM.value(1, 5) as reputation,
+--        dbms_random.string('A', 6) as contract_status,
+--        DBMS_RANDOM.value(0, 700) as injuries,
+--        DBMS_RANDOM.value(15, 50) as age,
+--        DBMS_RANDOM.value(15, 50) as wages,
+--        DBMS_RANDOM.value(0, 1000000000) as transfer_value,
+--        (SELECT id FROM CLUB SAMPLE(1) WHERE rownum = 1) as club_id
+--    from dual 
+--connect by level <=10
+
+alter table manager
+ENABLE constraint manager_club_fk;
+
+alter table club
+ENABLE constraint club_manager_fk;
